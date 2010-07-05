572e0020af6529823ae49dd7d62c8ec2b087107b
Matt Rude <m@mattrude.com>
2010-07-05 12:19:29 -0500

    new home page template for the main page

---
+++ home-page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * A custom page template without sidebar.
+ */
+
+get_header(); ?>
+
+		<div id="container" class="one-column">
+			<div id="content" role="main">
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-## -->
+
+				<?php comments_template( '', true ); ?>
+
+<?php endwhile; ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_footer(); ?>
